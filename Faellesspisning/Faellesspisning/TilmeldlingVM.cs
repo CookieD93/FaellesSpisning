8975248f9cce21dbb6b74d81e98a83e4ba634edf
cooki <cooki@DESKTOP-OV6U0MA>
2016-12-01 12:27:39 +0100

    Viewmodel til Tilmelding + relaycommands

---
+++ Faellesspisning/Faellesspisning/TilmeldlingVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faellesspisning
+{
+    class TilmeldlingVm : INotifyPropertyChanged
+    {
+        public TilmeldlingVm()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
