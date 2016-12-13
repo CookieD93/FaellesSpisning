18ef425299c99980e30531c5162b6d18e07248b1
Rude <Rude@LENOVO-PC>
2016-12-13 15:49:08 +0100

    Betaling

--- Faellesspisning/Faellesspisning/Betaling.cs
+++ Faellesspisning/Faellesspisning/Betaling.cs
@@ -8,6 +8,42 @@ namespace Faellesspisning
 {
     class Betaling
     {
-        public Double D { get; set; }
+        public Double DagligtUdlaegMandag { get; set; }
+        public Double DagligtUdlaegTirsdag { get; set; }
+        public Double DagligtUdlaegOnsdag { get; set; }
+        public Double DagligtUdlaegTorsdag { get; set; }
+
+        public Betaling()
+        {
+            DagligtUdlaegMandag = 0;
+            DagligtUdlaegTirsdag = 0;
+            DagligtUdlaegOnsdag =0;
+            DagligtUdlaegTorsdag = 1;
+        }
+
+        public double UgentligUdlæg()
+        {
+            return DagligtUdlaegMandag + DagligtUdlaegTirsdag + DagligtUdlaegOnsdag + DagligtUdlaegTorsdag;
+        }
+
+        public double KuvertPris()
+        {
+            return UgentligUdlæg()/ DeltagereIAlt() ;
+        }
+        public double DeltagerePerHus()
+        {
+            return 2;
+        }
+
+        public double DeltagereIAlt()
+        {
+            return 5;
+        }
+
+        public double HusBetaling()
+        {
+            return 10;
+        }
+
     }
 }
